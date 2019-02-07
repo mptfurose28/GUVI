21d54c2762ea2f5853b7352093353dd2383ee182
mptfurose28 <42862292+mptfurose28@users.noreply.github.com>
2019-02-07 11:46:20 +0530

    Update vowelorconsonant.py

--- vowelorconsonant.py
+++ vowelorconsonant.py
@@ -1,4 +1,4 @@
-X=str(raw_input("enter a letter"))
+X=str(raw_input())
 if(X in('a','e','i','o','u')):
 	print("Vowel")
 elif(X in('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z')):
